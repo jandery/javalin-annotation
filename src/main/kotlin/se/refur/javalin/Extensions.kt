daf3b994e3e283eafe1555799eb300afa29723f8
Jörgen Andersson <jorgen@hejmo.se>
2023-02-23 11:23:47 +0100

    Add classes

--- src/main/kotlin/se/refur/javalin/Extensions.kt
+++ src/main/kotlin/se/refur/javalin/Extensions.kt
@@ -4,6 +4,7 @@ import io.javalin.Javalin
 import org.reflections.scanners.Scanners
 import org.reflections.util.ClasspathHelper
 import org.reflections.util.ConfigurationBuilder
+import kotlin.reflect.KClass
 
 /**
  * The purpose of this extension function is to add endpoint handlers for web page requests
@@ -19,6 +20,19 @@ fun Javalin.exposePackageEndpoints(packageName: String): Javalin {
         // set what to search for
         .setScanners(Scanners.MethodsAnnotated)
 
+    // expose annotated methods
+    exposeMethods(configurationBuilder, this)
+    // Return this for chaining
+    return this
+}
+
+@Suppress("unused")
+fun <T : Any>Javalin.exposeClassEndpoints(clazz: KClass<T>): Javalin {
+    val configurationBuilder: ConfigurationBuilder = ConfigurationBuilder()
+        .addClassLoaders(clazz.java.classLoader)
+        .setScanners(Scanners.MethodsAnnotated)
+
+    // expose annotated methods
     exposeMethods(configurationBuilder, this)
     // Return this for chaining
     return this
@@ -28,14 +42,13 @@ fun Javalin.exposePackageEndpoints(packageName: String): Javalin {
  * The purpose of this function is to expose methods
  */
 private fun exposeMethods(configurationBuilder: ConfigurationBuilder, javalin: Javalin) {
-    getApiMethods(configurationBuilder)
-        .forEach { it.addHandler(javalin) }
-
-    getApiCookieMethods(configurationBuilder)
-        .forEach { it.addHandler(javalin) }
-
-    getPageMethods(configurationBuilder)
-        .forEach { it.addHandler(javalin) }
+    // Add handlers to webserver for Api annotations
+    getApiMethods(configurationBuilder).forEach { it.addHandler(javalin) }
+    // Add handlers to webserver for ApiCookie annotations
+    getApiCookieMethods(configurationBuilder).forEach { it.addHandler(javalin) }
+    // Add handlers to webserver for Page annotations
+    getPageMethods(configurationBuilder).forEach { it.addHandler(javalin) }
 }
+
 
 
